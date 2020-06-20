9b337abe298c18e8695825472600a338dae98add
Your Name <you@example.com>
2020-06-20 21:37:51 +0530

    last

--- piedpiper/js/script.js
+++ piedpiper/js/script.js
@@ -6,20 +6,38 @@
     var b="<div class='text-center'><img src='loading.gif'></div>";
     document.getElementById("images").innerHTML=b;
     });
-    
-        (function()
-          {
-            $ajaxUtils.sendGetRequest("js/name.txt", function(request)
-          {
-            var a=request.responseText;
-            document.getElementById("images").innerHTML=" ";
-            document.getElementById("images").innerHTML=a;
-          });
+    var textWrapper = document.querySelector('.ml6 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-          })();
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml6 .letter',
+    translateY: ["1.1em", 0],
+    translateZ: 0,
+    duration: 750,
+    delay: (el, i) => 50 * i
+  }).add({
+    targets: '.ml6',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+  document.getElementById("navbutton").addEventListener("blur",function()
+    {
+      $('.collapse').collapse('hide');
+    });
+        // (function()
+        //   {
+        //     $ajaxUtils.sendGetRequest("js/name.txt", function(request)
+        //   {
+        //     var a=request.responseText;
+        //     document.getElementById("images").innerHTML=" ";
+        //     document.getElementById("images").innerHTML=a;
+        //   });
+
+        //   })();
         
-      
-     
   })();
   
 
